f52ef28f72fc1cdd564bba7d09265e65c49ec3bb
himIoT19 <thehimu1990@gmail.com>
2020-07-17 04:01:18 +0530

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,9 +8,10 @@ router = DefaultRouter()
 
 # Register urls for HelloViewSet, UserProfileViewSet
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')  # base_name depricated in Django 3.0.8
-router.register('profile', views.UserProfileViewSet)                    # basename not required because we have queryset in UserProfileViewSet    
+router.register('profile', views.UserProfileViewSet)                    # basename not required because we have queryset in UserProfileViewSet
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
